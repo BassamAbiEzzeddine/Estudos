5c83c2831f7c9584e483ee7403bdce4b5082f624
Bassam Abi Ezzeddine <Bassamoliveira6@gmail.com>
2026-01-15 22:45:18 -0300

    atualizando besteiras

---
+++ 57.py
@@ -0,0 +1,13 @@
+from random import randint
+
+maquina = randint(0, 10)
+jogador = int(input("Insira um numero de 0 a 10: "))
+
+while maquina != jogador:
+    print("O numero escolhido esta incorreto{}".format(maquina))
+    jogador = int(input("Insira outro numero de 0 a 10: "))
+
+if jogador == maquina:
+    print("Parabens voce ganhou o numero da maquina era {}".format(maquina))
+
+print("Jogo encerrado")
