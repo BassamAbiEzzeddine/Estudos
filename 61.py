3d4c48ad618e4b515d0a6908f71287ff7459a3f5
Bassam Abi Ezzeddine <Bassamoliveira6@gmail.com>
2026-01-18 00:36:28 -0300

    Exercicios  61-62-63

---
+++ 61.py
@@ -0,0 +1,8 @@
+primeirotermo=int(input('Insira o primeiro termo da sua PA: '))
+razao=int(input('Insira a razao da sua PA: '))
+termos=10
+contas=primeirotermo
+while termos!=0:
+    print (contas, end=" ")
+    contas=contas+razao
+    termos=termos-1
